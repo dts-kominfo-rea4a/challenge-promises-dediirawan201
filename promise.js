c233c51896d75fe2e4ff24d0e12b65561cf36809
dediirawan201 <dediirwansyah1925@gmail.com>
2022-09-17 05:53:40 +0700

    tugas 2

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (respon) => {
+
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+
+  const allTheater = [...theaterIXX, ...theaterVGC]
+
+  let responPenonton = 0;
+
+  allTheater.forEach((film) => {
+    if (film.hasil === respon) {
+      responPenonton++;
+    }
+  })
+
+  return responPenonton;
+
+};
 
 module.exports = {
   promiseOutput,
